094e6a347250fc685496a8960d5c3f564e0185cb
c24024032 <elpulaa@cardiff.ac.uk>
2024-10-28 16:41:34 +0000

    created admin java file

---
+++ src/main/java/greenspace/controllers/controllers/Admin.java
@@ -0,0 +1,14 @@
+package greenspace.controllers.controllers;
+
+public class Admin {
+    public class ParkRequest {
+        private Long id;
+        private String name;
+        private String location;
+        private String description;
+    }
+
+
+
+
+}
