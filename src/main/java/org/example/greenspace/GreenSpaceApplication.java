e4e3a34c742cc055801777abc59253a6fe7d22ef
c2402996 <HuxtableZ@cardiff.ac.uk>
2024-10-28 10:44:53 +0000

    Initial commit

---
+++ src/main/java/org/example/greenspace/GreenSpaceApplication.java
@@ -0,0 +1,13 @@
+package org.example.greenspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenSpaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/greenspace/GreenSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.greenspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
